9e0c94445c3fb6435f4e29012cf13c6f42ae480c
Moth-0 <mikkelmb123@gmail.com>
2025-01-28 09:36:53 +0100

    Comp

---
+++ 4. Semester/IPSA/Aflevering1.py
@@ -0,0 +1,24 @@
+"""
+HANDIN 1 (down payment)
+
+This handin is done by: 
+    202307989 Mikkel Moth Billing 
+
+Reflection upon solution; 
+    I used int for the payment, so you cant pay with decimal increments. 
+    Off course you have to overshoot the loan payment, and therefore get an interger number of months,
+    i guess you could make it to calculate the last payment so you hit 0.0. 
+"""
+
+loan = float(input("Size of initial loan: "))
+rate = float(input("Monthly intrest rate: "))
+pay = int(input("Monthly payment: "))
+
+months = 0 
+
+while loan >= 0: 
+    months += 1
+    intrest = loan * rate
+    loan = loan + intrest - pay
+
+print("Months to pay off loan: " + str(months))
--- 4. Semester/IPSA/TØ1.py
+++
@@ -1,3 +0,0 @@
-x1 = int(input("Num 1: " ))
-x2 = int(input("Num 2: " ))
-print("Sum: " + str(x1+x2))
