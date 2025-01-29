679c15bc29f7fd05c5a08a9cdc436780856acff0
Moth-0 <mikkelmb123@gmail.com>
2025-01-29 16:12:33 +0100

    pc

---
+++ 4. Semester/IPSA/Aflevering 2.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+# Taget fra beskrivelsen: 
+from string import ascii_letters, digits
+s = open('saxo.txt', encoding='utf8').read()
+s = s.lower()
+s = ''.join([c for c in s if c in ascii_letters or c in digits])
+
+for 
