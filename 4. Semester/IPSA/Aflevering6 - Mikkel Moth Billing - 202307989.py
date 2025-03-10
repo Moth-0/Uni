756e815c524a432ac6dee0a39aa8120e78e1b7c9
Moth-0 <mikkelmb123@gmail.com>
2025-03-10 10:39:35 +0100

    cp

--- 4. Semester/IPSA/Aflevering6 - Mikkel Moth Billing - 202307989.py
+++ 4. Semester/IPSA/Aflevering6 - Mikkel Moth Billing - 202307989.py
@@ -12,6 +12,7 @@ Reflection upon solution:
 """
 
 class Person:
+    # Initialise Person
     def __init__(self, name=None, mother=None, father=None, born=None, died=None):
         self.name = name
         self.mother = mother
@@ -19,29 +20,34 @@ class Person:
         self.born = born
         self.died = died
 
+    # Change string output
     def __str__(self):
         return f"{self.name} {self.born}-{self.died if self.died else ""}"
     
+    # Get the parents with recursion
     def ancestors(self):
         return (str(self), 
                 self.father.ancestors() if self.father else "-", 
                 self.mother.ancestors() if self.mother else "-")
     
 class AnnotatedPerson(Person):
+    # Initialise Annotated with call to Person
     def __init__(self, name=None, mother=None, father=None, born=None, died=None, note=None):
         self.note = note 
         Person.__init__(self, name, mother, father, born, died)
 
+    # Add the note to the Person string 
     def __str__(self):
         return Person.__str__(self) + f" [{self.note}]"
 
-
+# Tree Print from excersise 9.2
 def print_tree(tree, pre=""):
     print(pre + "| -- " + tree[0]) 
     for child in tree[1:]:
         newpre = "|   " + pre 
         print_tree(child, newpre)
-      
+
+# Running example to see if it works       
 louise_af_HK = Person('Louise of Hessen-Kassel', None, None, 1817, 1898)
 christian_9  = Person('Christian 9.', None, None, 1818, 1906)
 louise_af_SN = AnnotatedPerson('Louise of Sweden-Norway', None, None, 1851, 1926, 'born Princess of Sweden and Norway')
